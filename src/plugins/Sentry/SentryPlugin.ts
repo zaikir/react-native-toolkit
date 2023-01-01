1abf92cd5732f9727a21723ecb329e2ca4234973
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-01-01 20:48:30 +0300

    fix

--- src/plugins/Sentry/SentryPlugin.ts
+++ src/plugins/Sentry/SentryPlugin.ts
@@ -1,4 +1,4 @@
-import Sentry from '@sentry/react-native';
+import * as Sentry from '@sentry/react-native';
 import { CaptureConsole } from '@sentry/integrations';
 import type { ReactNativeOptions } from '@sentry/react-native';
 import type { Plugin } from 'plugins/Plugin';
@@ -12,11 +12,12 @@ export class SentryPlugin implements Plugin {
 
   async init() {
     if (!process.env.SENTRY_DSN && !this.options?.dsn) {
-      throw new Error('Sentry DSN required. Pass it using SENTRY_DSN env variable or via constructor');
+      throw new Error('Sentry DSN required. Use SENTRY_DSN env variable or pass it in the constructor');
     }
 
     Sentry.init({
-      dsn: process.env.SENTRY_DSN,
+      dsn: this.options?.dsn || process.env.SENTRY_DSN,
+      // @ts-ignore
       integrations: [new CaptureConsole({ levels: ['warn', 'error'] })],
       ...this.options,
     });
