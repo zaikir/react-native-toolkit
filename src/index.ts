3941ba19492475777c1ed215c7bc7e0598de26a5
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-01-05 09:04:15 +0300

    improve tree shaking

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,6 @@ export { AlertsProvider, DropdownAlert } from './contexts/AlertsContext';
 
 // plugins
 export { Plugin } from './plugins/Plugin';
-export { RemoteConfig } from './plugins/types';
-export { SentryPlugin } from './plugins/Sentry';
-export { FirebaseRemoteConfig } from './plugins/FirebaseRemoteConfig';
 
 // hooks
 export { default as useAlerts } from './hooks/useAlerts';
