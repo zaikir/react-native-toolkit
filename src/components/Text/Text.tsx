d47982a6f1943f7dfe413a9f5eb76edabff2bb23
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-01-20 12:01:26 +0300

    update Text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,12 +1,38 @@
-import type { TextVariant } from 'theme';
+import React, { useMemo } from 'react';
+import { StyleProp, Text as TextBase } from 'react-native';
+import type { TextStyle } from 'react-native';
+import { useTheme } from 'index';
+import { getFontWeightName } from './utils/getFontWeightName';
 
-type Props = {
-  variant: TextVariant
+type TextProps = TextStyle & {
+  variant?: 'default',
+  style?: StyleProp<TextStyle>
 };
 
-export default function Text(props: Props) {
-  const { variant } = props;
-  console.log(variant);
+export default function Text(props: TextProps) {
+  const theme = useTheme();
 
-  return null;
+  const fontAssetName = useMemo(() => {
+    // @ts-ignore
+    const weights = theme.fonts[props.fontFamily];
+    const names = getFontWeightName(props.fontWeight);
+
+    const name = weights.find((x: string) => names.includes(x.toLowerCase()));
+    if (!name) {
+      throw new Error(`Unknown font weight "${name}" for font family "${props.fontFamily}"`);
+    }
+
+    return `${props.fontFamily}-${name}`;
+  }, [theme, props.fontWeight, props.fontFamily]);
+
+  const style = props.style && 'length' in props.style
+    ? [...props.style, { fontFamily: fontAssetName }]
+    : { ...props.style && props.style as any, fontFamily: fontAssetName };
+
+  return (
+    <TextBase
+      {...props}
+      style={style}
+    />
+  );
 }
---
+++ src/components/Text/utils/getFontWeightName.ts
@@ -0,0 +1,20 @@
+import type { TextStyle } from 'react-native';
+
+export function getFontWeightName(fontWeight: TextStyle['fontWeight']) {
+  switch (fontWeight) {
+    case '100': return ['hairline', 'thin'];
+    case '200': return ['extralight', 'ultralight'];
+    case '300': return ['light'];
+    case '400':
+    case 'normal':
+      return ['normal', 'regular'];
+    case '500': return ['medium'];
+    case '600': return ['semibold', 'demibold'];
+    case '700':
+    case 'bold':
+      return ['bold'];
+    case '800': return ['extrabold', 'ultrabold'];
+    case '900': return ['black', 'heavy'];
+    default: throw new Error(`Unknown font weight: ${fontWeight}`);
+  }
+}
