c974fa34dd95eca70cde4647fce325669b45ec70
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-02-10 10:11:52 +0300

    handle error

--- src/plugins/FirebasePlugin/FirebasePlugin.ts
+++ src/plugins/FirebasePlugin/FirebasePlugin.ts
@@ -1,5 +1,6 @@
 import initializeRemoteConfig from '@react-native-firebase/remote-config';
 
+import { timeout } from 'index';
 import { Plugin, PluginFeature } from 'plugins/Plugin';
 import type { IRemoteConfigPlugin, RemoteConfig } from 'plugins/types';
 
@@ -23,43 +24,49 @@ export class FirebasePlugin extends Plugin implements IRemoteConfigPlugin {
     if (this._remoteConfig) {
       const config = initializeRemoteConfig();
 
-      await config.setConfigSettings({ minimumFetchIntervalMillis: 0 });
-      await config.setDefaults({ ...this._remoteConfig });
+      try {
+        await timeout(async () => {
+          await config.setConfigSettings({ minimumFetchIntervalMillis: 0 });
+          await config.setDefaults({ ...this._remoteConfig });
 
-      await config.fetch(0);
-      await config.activate();
+          await config.fetch(0);
+          await config.activate();
+        });
 
-      this._remoteConfig = Object.fromEntries(
-        Object.entries(config.getAll()).map(([key, entry]) => {
-          // @ts-ignore
-          const defaultValue = this._remoteConfig[key];
+        this._remoteConfig = Object.fromEntries(
+          Object.entries(config.getAll()).map(([key, entry]) => {
+            // @ts-ignore
+            const defaultValue = this._remoteConfig[key];
 
-          if (!defaultValue) {
-            try {
-              // @ts-ignore
-              const parsed = JSON.parse(entry._value);
-              return [key, parsed];
-            } catch {
+            if (!defaultValue) {
+              try {
+                // @ts-ignore
+                const parsed = JSON.parse(entry._value);
+                return [key, parsed];
+              } catch {
+                return [key, entry.asString()];
+              }
+            }
+
+            if (typeof defaultValue === 'string') {
               return [key, entry.asString()];
             }
-          }
 
-          if (typeof defaultValue === 'string') {
-            return [key, entry.asString()];
-          }
+            if (typeof defaultValue === 'boolean') {
+              return [key, entry.asBoolean()];
+            }
 
-          if (typeof defaultValue === 'boolean') {
-            return [key, entry.asBoolean()];
-          }
+            if (typeof defaultValue === 'number') {
+              return [key, entry.asNumber()];
+            }
 
-          if (typeof defaultValue === 'number') {
-            return [key, entry.asNumber()];
-          }
-
-          // @ts-ignore
-          return [key, JSON.parse(entry._value)];
-        }),
-      );
+            // @ts-ignore
+            return [key, JSON.parse(entry._value)];
+          }),
+        );
+      } catch {
+        // no op
+      }
     }
   }
 }
