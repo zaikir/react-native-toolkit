e0ed5a0090531ed77fa59e30c1d7c824d3813241
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-08-18 22:27:25 +0300

    add delay

--- src/plugins/IdfaPlugin/IdfaPlugin.ts
+++ src/plugins/IdfaPlugin/IdfaPlugin.ts
@@ -2,6 +2,8 @@ import ReactNativeIdfaAaid from '@sparkfabrik/react-native-idfa-aaid';
 
 import { Plugin, PluginFeature } from 'plugins/Plugin';
 
+import { wait } from '../../utils/promise/wait';
+
 export class IdfaPlugin extends Plugin {
   readonly name = 'IdfaPlugin';
   readonly features: PluginFeature[] = ['IDFA'];
@@ -9,11 +11,19 @@ export class IdfaPlugin extends Plugin {
 
   private _idfa: string | null = null;
 
+  constructor(readonly options: { delay?: number }) {
+    super();
+  }
+
   public get idfa() {
     return this._idfa;
   }
 
   async initialize() {
+    if (this.options.delay) {
+      await wait(this.options.delay);
+    }
+
     const res = await ReactNativeIdfaAaid.getAdvertisingInfo();
     if (!res.id) {
       return;
