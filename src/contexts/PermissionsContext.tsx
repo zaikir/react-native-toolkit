457a9d7bc0a19c48c306fa8b8cc233aa24492fdf
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-10-11 00:53:47 +0300

    add usePermissions hook

---
+++ src/contexts/PermissionsContext.tsx
@@ -0,0 +1,58 @@
+import React, {
+  createContext,
+  Dispatch,
+  PropsWithChildren,
+  SetStateAction,
+  useMemo,
+  useState,
+} from 'react';
+import { StatusBar, useColorScheme, StatusBarProps } from 'react-native';
+
+export type ColorScheme = 'light' | 'dark';
+
+export type ColorSchemeContextType = {
+  colorScheme: ColorScheme;
+  setColorScheme: Dispatch<SetStateAction<ColorScheme | null>>;
+};
+
+export const ColorSchemeContext = createContext<ColorSchemeContextType>(
+  {} as any,
+);
+
+export function ColorSchemeProvider({
+  scheme,
+  children,
+  statusBarProps,
+}: PropsWithChildren<{
+  scheme?: ColorScheme;
+  statusBarProps?: StatusBarProps;
+}>) {
+  const systemColorScheme = useColorScheme();
+  const [fixedScheme, setFixedScheme] = useState<ColorScheme | null>(
+    scheme ?? null,
+  );
+
+  const contextData = useMemo<ColorSchemeContextType>(
+    () => ({
+      colorScheme: fixedScheme ?? systemColorScheme ?? 'light',
+      setColorScheme: setFixedScheme,
+    }),
+    [systemColorScheme, fixedScheme],
+  );
+
+  return (
+    <ColorSchemeContext.Provider value={contextData}>
+      <StatusBar
+        {...statusBarProps}
+        barStyle={
+          statusBarProps?.barStyle ??
+          (contextData.colorScheme === 'light'
+            ? 'dark-content'
+            : 'light-content')
+        }
+        animated={statusBarProps?.animated ?? true}
+      />
+      {children}
+    </ColorSchemeContext.Provider>
+  );
+}
---
+++ src/hooks/usePermissions.ts
@@ -0,0 +1,26 @@
+import { useCallback } from 'react';
+import { check, Permission, request } from 'react-native-permissions';
+
+export function usePermissions() {
+  const isPermissionGranted = useCallback(async (permission: Permission) => {
+    const status = await check(permission);
+    if (status === 'unavailable' || status === 'blocked') {
+      return false;
+    }
+
+    if (status === 'granted' || status === 'limited') {
+      return true;
+    }
+
+    const newStatus = await request(permission);
+    if (newStatus === 'granted' || newStatus === 'limited') {
+      return true;
+    }
+
+    return false;
+  }, []);
+
+  return {
+    isPermissionGranted,
+  };
+}
