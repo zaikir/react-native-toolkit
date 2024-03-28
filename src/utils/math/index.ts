84490d94eee96fb2ae4660cbb1b5b8ba0aa73adf
Kirill Zaidulin <zaidulinkirill@gmail.com>
2024-03-28 10:21:41 +0300

    add shuffle util

--- src/utils/math/index.ts
+++ src/utils/math/index.ts
@@ -1,6 +1,8 @@
 import { randomInt, randomFloat } from './random';
+import { shuffle } from './shuffle';
 
 export class MathUtils {
   static randomInt = randomInt;
   static randomFloat = randomFloat;
+  static shuffle = shuffle;
 }
---
+++ src/utils/math/shuffle.ts
@@ -0,0 +1,10 @@
+export function shuffle<T>(array: T[]) {
+  const arr = [...array];
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+}
