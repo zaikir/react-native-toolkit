3b30c3c6960031e2f4da6f05ce33f8a7f6cfa8ba
Kirill Zaidulin <zaidulinkirill@gmail.com>
2024-04-04 17:19:03 +0300

    fix plist generation

--- scripts/env/index.js
+++ scripts/env/index.js
@@ -17,6 +17,37 @@ function pathAndroidProguard(env) {
   fs.writeFileSync(filename, content, 'utf-8');
 }
 
+const GoogleServicePlist = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>API_KEY</key>
+	<string>[API_KEY]</string>
+	<key>GCM_SENDER_ID</key>
+	<string>[GCM_SENDER_ID]</string>
+	<key>PLIST_VERSION</key>
+	<string>1</string>
+	<key>BUNDLE_ID</key>
+	<string>[BUNDLE_ID]</string>
+	<key>PROJECT_ID</key>
+	<string>[PROJECT_ID]</string>
+	<key>STORAGE_BUCKET</key>
+	<string>[STORAGE_BUCKET]</string>
+	<key>IS_ADS_ENABLED</key>
+	[IS_ADS_ENABLED]
+	<key>IS_ANALYTICS_ENABLED</key>
+  [IS_ANALYTICS_ENABLED]
+	<key>IS_APPINVITE_ENABLED</key>
+	<true></true>
+	<key>IS_GCM_ENABLED</key>
+	<true></true>
+	<key>IS_SIGNIN_ENABLED</key>
+	<true></true>
+	<key>GOOGLE_APP_ID</key>
+	<string>[GOOGLE_APP_ID]</string>
+</dict>
+</plist>`;
+
 function copyGoogleServices(env) {
   const rootFileContent = fs.readFileSync('src/Root.tsx', 'utf-8');
 
@@ -30,36 +61,17 @@ function copyGoogleServices(env) {
 
   const filename = 'ios/GoogleService-Info.plist';
 
-  const plistFileContent = fs
-    .readFileSync(filename, 'utf-8')
-    .replace(
-      /<key>API_KEY<\/key>\n	<string>.*<\/string>/m,
-      `<key>API_KEY</key>\n	<string>${env.FIREBASE_API_KEY}</string>`,
-    )
-    .replace(
-      /<key>GCM_SENDER_ID<\/key>\n	<string>.*<\/string>/m,
-      `<key>GCM_SENDER_ID</key>\n	<string>${env.FIREBASE_GCM_SENDER_ID}</string>`,
-    )
-    .replace(
-      /<key>BUNDLE_ID<\/key>\n	<string>.*<\/string>/m,
-      `<key>BUNDLE_ID</key>\n	<string>${env.APP_BUNDLE_ID}</string>`,
-    )
-    .replace(
-      /<key>PROJECT_ID<\/key>\n	<string>.*<\/string>/m,
-      `<key>PROJECT_ID</key>\n	<string>${env.FIREBASE_PROJECT_ID}</string>`,
-    )
-    .replace(
-      /<key>STORAGE_BUCKET<\/key>\n	<string>.*<\/string>/m,
-      `<key>STORAGE_BUCKET</key>\n	<string>${env.FIREBASE_PROJECT_ID}.appspot.com</string>`,
-    )
-    .replace(
-      /<key>IS_ANALYTICS_ENABLED<\/key>\n	<false><\/false>/m,
-      `<key>IS_ANALYTICS_ENABLED</key>\n	<true></true>`,
-    )
-    .replace(
-      /<key>GOOGLE_APP_ID<\/key>\n	<string>.*<\/string>/m,
-      `<key>GOOGLE_APP_ID</key>\n	<string>${env.FIREBASE_GOOGLE_APP_ID}</string>`,
-    );
+  const plistFileContent = GoogleServicePlist.replace(
+    '[API_KEY]',
+    env.FIREBASE_API_KEY,
+  )
+    .replace('[GCM_SENDER_ID]', env.FIREBASE_GCM_SENDER_ID)
+    .replace('[BUNDLE_ID]', env.APP_BUNDLE_ID)
+    .replace('[PROJECT_ID]', env.FIREBASE_PROJECT_ID)
+    .replace('[STORAGE_BUCKET]', `${env.FIREBASE_PROJECT_ID}.appspot.com`)
+    .replace('[IS_ADS_ENABLED]', `<false></false>`)
+    .replace('[IS_ANALYTICS_ENABLED]', `<true></true>`)
+    .replace('[GOOGLE_APP_ID]', env.FIREBASE_GOOGLE_APP_ID);
 
   fs.writeFileSync(filename, plistFileContent, 'utf-8');
 }
